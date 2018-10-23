89ec2d519a6efaec3669aec06c39b0d39e4698e4
Adi Hayat <adi.hayat@mobileye.com>
2018-10-23 19:33:30 +0300

    move version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mpreader',
-      version='0.6',
+      version='0.7',
       description='multiprocess batch reader for DNN training',
       url='https://github.com/adihayat/mpreader.git',
       author='adi hayat',
